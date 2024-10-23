881e77b7330e96f0595b354a98ad13876724729a
Olesya Figurovskaya <lisonkadrug@gmail.com>
2024-10-23 16:22:44 +0300

    components

---
+++ src/components/GameWord.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+interface Props {
+    word: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+    <div class="word">
+        <span class="letter" v-for="(letter, index) in word" :key="index"></span>
+    </div>
+</template>
