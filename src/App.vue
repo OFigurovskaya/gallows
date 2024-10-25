6b046cc8fecd8926c5b5b25b3edf539f7991a2f8
Olesya Figurovskaya <lisonkadrug@gmail.com>
2024-10-25 15:22:37 +0300

    GameWord added

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,29 @@ import GameWrong from "./components/GameWrong.vue";
 import GameWord from "./components/GameWord.vue";
 import GamePopup from "./components/GamePopup.vue"
 import GameNotofication from "./components/GameNotofication.vue"
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const word = ref('Василий');
+const word = ref<string>('игорь');
+const letters = ref<string[]>([]);
+const correctLetters = computed(() => letters.value.filter(x => word.value.includes(x)))
+
+window.addEventListener('keydown', ({ key }) => {
+    if(/[а-яА-ЯеЁ]/.test(key)) {
+      letters.value.push(key.toLowerCase())
+  }
+})
 </script>
 
 <template>
+  {{ word }}
+  {{ letters }}
+  {{ correctLetters }}
   <div id="app">
     <GameHeader />
     <div class="game-container">
       <GameFigure />
       <GameWrong />
-      <GameWord :word="word"/>
+      <GameWord :word="word" :correct-letters="correctLetters"/>
     </div>
     <!-- Container for final message -->
     <GamePopup v-if="false"/>
